7d12803fa219c6cf6b6b3e18dbf2587980985522
takeuchi7 <bushi@kwansei.ac.jp>
2019-05-24 15:29:20 +0900

    0524

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'r5jb-@uz*99cg03e@=!$&hg$-3n4mkjwu9g#g1$y0(!@5r=_-x'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1','.pythonanywhere.com']
 
